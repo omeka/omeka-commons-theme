5c3b82727a888a10ad87d60fe1300b714cad1842
Kim Nguyen <design@kimisgold.com>
2013-08-19 11:22:38 -0400

    First commit.

---
+++ common/header.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html class="<?php echo get_theme_option('Style Sheet'); ?>" lang="<?php echo get_html_lang(); ?>">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php if ($description = option('description')): ?>
+    <meta name="description" content="<?php echo $description; ?>">
+    <?php endif; ?>
+
+    <?php
+    if (isset($title)) {
+        $titleParts[] = strip_formatting($title);
+    }
+    $titleParts[] = option('site_title');
+    ?>
+    <title><?php echo implode(' &middot; ', $titleParts); ?></title>
+
+    <?php echo auto_discovery_link_tags(); ?>
+
+    <!-- Plugin Stuff -->
+    <?php fire_plugin_hook('public_head', array('view'=>$this)); ?>
+
+    <?php queue_css_url('http://fonts.googleapis.com/css?family=Raleway:400,600'); ?> 
+    <?php queue_css_file('screen'); ?>
+    <?php echo head_css(); ?>
+    
+</head>
+
+<?php echo body_tag(array('id' => @$bodyid, 'class' => @$bodyclass)); ?>
+
+    <header>
+        <div class="container">
+            <div class="site-title"><?php echo link_to_home_page(theme_logo()); ?></div>
+
+            <nav>
+                <?php echo public_nav_main(); ?>
+            </nav>
+                
+            <div class="user">
+                <span class="register"><a href="#">Register</a></span>
+                <div class="login">
+                    <span><a href="#">Login</a></span>
+                    <form>
+                        <input type="text" placeholder="Name" />
+                        <input type="password" />
+                    </form>
+                </div>
+            </div>
+
+        </div>
+        
+        <?php if (@$bodyid !== 'home'): ?>
+        <?php echo search_form(); ?>
+        <?php endif; ?>
+    </header>
+    
+    <div role="main">
