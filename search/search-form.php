557903ba641edd73580d2418fd392e3e9e12a836
Kim Nguyen <design@kimisgold.com>
2013-12-12 17:56:36 -0500

    Contain footer on homepage.

--- search/search-form.php
+++ search/search-form.php
@@ -5,7 +5,7 @@
     <?php echo $this->formText('query', $filters['query'], array('placeholder' => 'Search the whole site')); ?>
     <fieldset id="record-types">
         <?php foreach ($record_types as $key => $value): ?>
-            <a href="#" class="record-type"><?php echo $this->formCheckbox('record_types[]', $key, in_array($key, $filters['record_types']) ? array('checked' => true, 'id' => 'record_types-' . $key, 'tabindex' => '-1') : array('tabindex' => '-1')); ?> <?php echo $value; ?></a>
+            <a href="#" class="record-type" tabindex="0"><?php echo $this->formCheckbox('record_types[]', $key, in_array($key, $filters['record_types']) ? array('checked' => true, 'id' => 'record_types-' . $key, 'tabindex' => '-1') : array('tabindex' => '-1')); ?> <?php echo $value; ?></a>
         <?php endforeach; ?>
     </fieldset>
     <button type="submit" value="Search"><span class="icon-search"></span><span class="screen-reader-text"><?php echo __('Search'); ?></span></button>
